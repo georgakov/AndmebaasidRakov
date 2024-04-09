5bcf24c3662eef5adcee903ba51cf14e33c923bb
Georg Rakov <georg.rakov3@gmail.com>
2024-04-09 14:48:22 +0300

    git käsud on konspekte

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -43,3 +43,45 @@ nimetus, kestvus, algus_kuupaev, kerjeldus)
 VALUES (
 'Kell', 200, '2024-04-1', 'Kellll moodul');
 SELECT * FROM oppeaine;
+
+
+--Tabel opetaja
+
+USE rakovTITpv22;
+create table opetaja(
+opetajaID int Primary key identity(1,1),
+eesnimi varchar(50),
+perekonnanimi varchar(50) NOT NULL,
+sunniaeg date,
+kontakt varchar(50));
+select * from opetaja;
+insert into opetaja(eesnimi, perekonnanimi, kontakt)
+values('Mihail', 'Agapov', '79104444116');
+select * from opetaja;
+
+insert into opetaja(eesnimi, perekonnanimi, kontakt)
+values('Irina', 'Merkulova', '@telegram');
+select * from opetaja;
+
+--andmete uuendamine
+UPDATE opetaja SET sunniaeg='1999-10-10'
+where opetajaID=1;
+--FK-->PK
+--tabeli muutmine veergu lisamine
+Alter table oppeaine ADD opetajaID int;
+Select * from oppeaine;
+SELECT * from opetaja;
+UPDATE oppeaine SET	kirjeldus='hard';
+
+--Foreign key lisamine tabeli sisse
+Alter Table oppeaine
+ADD FOREIGN KEY (opetajaID) REFERENCES opetaja(opetajaID)
+
+INSERT INTO oppeaine(
+nimetus, kestvus, kirjeldus, algus_kuupaev, opetajaID)
+VALUES (
+'Linux', 500, '1', '2024-04-9', '1');
+SELECT * FROM oppeaine;
+
+
+
